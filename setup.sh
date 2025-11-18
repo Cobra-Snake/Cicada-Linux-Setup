308a2c0488856e790dfc9dd430962819dccff3e8
Cobra-Snake <anaselgyar383@gmail.com>
2025-11-18 22:58:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 apt update -y && apt-get update -y && apt upgrade -y
 
 
-tools=("gobuster" "ghex" "steghide" "yersinia")
+tools=("gobuster" "ghex" "steghide" "yersinia" "amass" "aircrack-ng" "apktool" "arping" "hping3" "beef-xss" "bloodhound" "bully" "cadaver" "cewl" "0trace" "ltrace" "nuclei" "dirb" "dirsearch" "dirbuster" "netexec" "crackmapexec" "evil-winrm" "crunch" "legion" "set" "recon-ng" "theharvester" "ettercap" "dnsrecon" "sherlock" "maltego" "radare2" "wafw00f" "subfinder" "spiderfoot" "macchanger" "foremost" "stegcracker" "wfuzz" "enum4linux-ng" "dmitry" "socat" "smtp-user-enum" "smbmap" "impacket" "medusa" "wine" "gospider" "p0f" "davtest" "powersploit" "sslsplit" "sslstrip" "zaproxy" "peass-ng" "cloud-enum" "gophish" "httrack" "onesixtyone" "wapiti" "whatweb" "webshells" "lbd" "ncrack" "tmux" "raven" "tinja" "fierce") 
 
 for i in "${tools[@]}"; do
     apt install "$i" -y --fix-missing
 done
 
+wget "https://github.com/bee-san/RustScan/releases/download/2.4.1/rustscan.deb.zip"
+dpkg -i rustscan.deb
