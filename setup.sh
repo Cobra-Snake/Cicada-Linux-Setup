bfca22c77e8e021b32c37a2c3ee252931daa2406
Cobra-Snake <anaselgyar383@gmail.com>
2026-01-26 15:43:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,13 +9,13 @@ tools=(
 "theharvester" "ettercap-graphical" "dnsrecon" "sherlock" "maltego"
 "radare2" "wafw00f" "subfinder" "spiderfoot" "macchanger" "foremost"
 "stegcracker" "wfuzz" "enum4linux-ng" "dmitry" "socat" "pdfcrack"
-"smtp-user-enum" "smbmap" "impacket-scripts" "medusa" "wine"
+"smtp-user-enum" "smbmap" "impacket-scripts" "medusa" "wine" "xsser"
 "gospider" "p0f" "davtest" "powersploit" "sslsplit" "sslstrip"
 "zaproxy" "peass" "cloud-enum" "gophish" "httrack" "onesixtyone"
 "wapiti" "whatweb" "webshells" "lbd" "ncrack" "tmux" "raven" "tinja" "fierce" 
 "wpscan" "set" "fping" "dsniff" "dnschef" "masscan" "feroxbuster" "samdump2" "shellter" 
 "spraykatz" "Wordlists" "pspy" "dmitry" "dumpzilla" "kerberoast" "neofetch" "hashid" 
-"spooftooph" "ghidra" "remmina" "mimikatz" "commix" "binwalk")
+"spooftooph" "ghidra" "remmina" "mimikatz" "commix" "binwalk" "reaver" "rubeus")
 
 for i in "${tools[@]}"; do
     echo "[+] Installing $i ..."
@@ -25,22 +25,24 @@ done
 wget "https://github.com/bee-san/RustScan/releases/download/2.4.1/rustscan.deb.zip"
 unzip rustscan.deb.zip
 dpkg -i rustscan_2.4.1-1_amd64.deb
-rm rustscan.deb.zip rustscan_2.4.1-1_amd64.deb rustscan.tmp0-stripped
 gem install haiti-hash
 echo "neofetch --ascii_distro kali" >> ~/.zshrc
 
 apt install python2 -y --fix-missing
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 python2 get-pip.py
-rm get-pip.py
 pip2 --version
 
 git clone "https://github.com/Screetsec/TheFatRat.git" && cd TheFatRat
 chmod +x update && ./update && chmod +x setup.sh && ./setup.sh
 mv TheFatRat /root
+cd ..
 
 wget https://update.code.visualstudio.com/latest/linux-deb-x64/stable -O vscode.deb
 dpkg -i vscode.deb
-rm vscode.deb
 
 git clone "https://github.com/screetsec/Dracnmap.git" && cd Dracnmap
+chmod +x dracnmap-v2.2.sh
+mv dracnmap-v2.2.sh dracnmap
+mv dracnmap /usr/local/bin
+cd ..
