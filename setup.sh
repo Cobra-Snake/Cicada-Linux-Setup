fc167910889f5fc4840d0f586a65bc456078f171
0xCobra <anaselgyar383@gmail.com>
2026-03-19 17:46:50 +0200

    Fix typo in setup.sh package list

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ tools=(
 "arping" "hping3" "beef-xss" "bloodhound" "bully" "cadaver" "cewl" "cmseek"
 "ltrace" "nuclei" "dirb" "dirsearch" "dirbuster" "netexec" "fastfetch"
 "crackmapexec" "evil-winrm" "crunch" "legion" "set" "recon-ng" "sublist3r"
-"theharvester" "ettercap-graphical" "dnsrecon" "sherlock" "maltego" "bloodyAD"4
+"theharvester" "ettercap-graphical" "dnsrecon" "sherlock" "maltego" "bloodyAD"
 "radare2" "wafw00f" "subfinder" "spiderfoot" "macchanger" "foremost" "cupp"
 "stegcracker" "wfuzz" "enum4linux-ng" "dmitry" "socat" "pdfcrack" "nishang"
 "smtp-user-enum" "smbmap" "impacket-scripts" "medusa" "wine" "xsser" "knock"
